6d352cbc1db17ac7afc2f3f40d7ed69d9fca88d2
Lobbyism <Yvan.Ndong.Ekouaga@USherbrooke.ca>
2024-12-03 23:31:12 -0500

    feat: Add book controller

---
+++ Projet/Projet/BookController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet
+{
+    public class BookController
+    {
+        private readonly BookRepository BookRepository;
+        public BookController(BookRepository bookRepository)
+        {
+            BookRepository = bookRepository;
+        }
+        public List<Book> GetBooks() => BookRepository.GetAllBooks();
+        public void AddBook(Book book) => BookRepository.AddBook(book);
+        public void RemoveBook(Book book) => BookRepository.RemoveBook(book);
+    }
+}
