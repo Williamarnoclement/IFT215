e5fc9bbee2acca8b7d1a10232e2a5826b1b94800
Lobbyism <Yvan.Ndong.Ekouaga@USherbrooke.ca>
2024-12-03 23:30:57 -0500

    feat: Add book model

---
+++ Projet/Projet/BookRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet
+{
+    public class BookRepository
+    {
+        private List<Book> books = new List<Book>();
+        public event Action? BooksChanged;
+        public List<Book> GetAllBooks() => books;
+        public void AddBook(Book book)
+        {
+            books.Add(book);
+            BooksChanged?.Invoke();
+        }
+        public void RemoveBook(Book book)
+        {
+            books.Remove(book);
+            BooksChanged?.Invoke();
+        }
+    }
+}
