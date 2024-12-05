92dd12399b74fd9631571fb54f14ab5cf565da46
beal1410 <beal1410@usherbrooke.ca>
2024-12-04 20:32:39 -0500

    ajout UserController et Repository

---
+++ Projet/Projet/UserController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Projet
+{
+    public class UserController
+    {
+        private readonly UserRepository UserRepository;
+
+        public UserController(UserRepository userRepository)
+        {
+            UserRepository = userRepository;
+        }
+
+        public List<User> GetUsers() => UserRepository.GetAllUsers();
+
+        public User? GetUser(int id) => UserRepository.GetUserById(id);
+
+        public List<User> GetAllUsers()
+        {
+            return UserRepository.GetAllUsers(); // Suppose que GetAllUsers() existe dans UserRepository
+        }
+
+        public void AddUser(User user)
+        {
+            if (user != null)
+                UserRepository.AddUser(user);
+        }
+
+        public void RemoveUser(User user)
+        {
+            if (user != null)
+                UserRepository.RemoveUser(user);
+        }
+    }
+}
---
+++ Projet/Projet/UserRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Projet
+{
+    public class UserRepository
+    {
+        private List<User> Users = new List<User>();
+        public event Action? UsersChanged;
+
+        public List<User> GetAllUsers() => Users;
+
+        public User? GetUserById(int id) => Users.FirstOrDefault(u => u.Id == id);
+
+        public void AddUser(User user)
+        {
+            if (!Users.Any(u => u.Id == user.Id)) // Eviter les doublons
+            {
+                Users.Add(user);
+                UsersChanged?.Invoke();
+            }
+        }
+
+        public void RemoveUser(User user)
+        {
+            Users.Remove(user);
+            UsersChanged?.Invoke();
+        }
+    }
+}
